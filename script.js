bb09064e5e47fc7aca9cc7b7aa792412aa880933
LIN-YOU-AN <109137140+LIN-YOU-AN@users.noreply.github.com>
2022-07-12 16:07:30 +0800

    v1

---
+++ script.js
@@ -0,0 +1,9 @@
+<script>
+//      function change() {
+//      document.body.style.background = document.getElementById("colors").value
+//   }  
+
+ document.getElementById("colors").addEventListener("change",function(){
+  document.body.style.background = document.getElementById("colors").value
+ })
+ </script>
